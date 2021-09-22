54b02ada48335622c0c57a63c73e99a0fc261886
tartarot <Eduardslazar@gmail.com>
2021-09-22 12:03:25 -0500

    cambios

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <div class="Video">
         <div class="contbody">
-            Videos
+            <iframe width="100%" height="50%" src="https://www.youtube.com/watch?v=9IZTss4ud1k&ab_channel=GameSpot"  allowfullscreen></iframe>
         </div>
             <div class="contbody">
                 <div class="contgrid">
@@ -12,7 +12,8 @@
                         <h1>Fecha</h1>
                     </div>
                 </div>
-                <br>
+            </div>
+            <div class="contbody">
                 <div class="contgrid">
                     <div>
                         <h1>Descripcion</h1>
@@ -22,10 +23,10 @@
                     </div>
                 </div>
             </div>
-        <div class="contbody">
-            comentarios
+            <div class="contbody">
+                comentarios
+            </div>
         </div>
-    </div>
 </template>
 <script>
 export default {
@@ -38,11 +39,13 @@ export default {
 .contbody {
     display: flex;
     justify-content: space-between;
-    margin: 50px;
+    margin-bottom: 0px;
 }
 .contgrid {
     display: flex;
     justify-content: space-between;
+    margin: 0%;
+    width: 100%;
     
 }
 </style>
