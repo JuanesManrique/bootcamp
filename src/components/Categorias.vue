6ee08abbee54300aa045ed229467ffa05f82e2b9
Juan Sanchez <juansanchezmanrique@gmail.com>
2021-09-21 22:14:03 -0500

    cambios

---
+++ src/components/Categorias.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "Categorias"
+}
+</script>
+<style scoped>
+
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,37 @@
-vue <template>
-  <div>
-    <h1>hola</h1>
+<template>
+  <div class="navbar w-full h-16 px-4 py-1 border-b flex items-center">
+    <div class="content-label flex items-center">
+      <label for='collapsable' class='button'><Menu /></label>
+    </div>
   </div>
 </template>
 <script>
+import Menu from './icons/Menu.vue'
+
 export default {
   name: "Navbar",
+  components: {
+    Menu,
+  }
 }
 </script>
-<style scoped>
-  
+<style>
+  .navbar {
+    background-color: white;
+    position: sticky;
+    top: 0;
+  }
+
+  .content-label{
+    width: 100%;
+    height: 100%;
+    transition: background-color 0.5s ease-in-out;
+  }
+
+  .button {
+    cursor: pointer;
+    font-weight: 800;
+    color: black;
+    font-size: 40px;
+  }
 </style>
